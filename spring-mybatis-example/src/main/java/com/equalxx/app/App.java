f62e268e01980746a8d3b061046771cdf9eb8232
薛雪 <xuexue@localhost>
2021-02-22 00:28:55 +0800

    add App

--- spring-mybatis-example/src/main/java/com/equalxx/app/App.java
+++ spring-mybatis-example/src/main/java/com/equalxx/app/App.java
@@ -1,4 +1,9 @@
 package com.equalxx.app;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.equalxx")
 public class App {
 }
